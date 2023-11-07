fce34cb99d6e97ab3ea18cad6355e543a54d0005
Shlok Nangia <shlok_nangia@sfu.ca>
2023-11-07 15:01:46 -0800

    updating docs

---
+++ docs/assets/css/css1.ts
@@ -0,0 +1,8 @@
+export const css = [ "<iframe", 'src=url','loading="lazy"', 'style="width:', "100%;", "height:", "600px;", "border:", "0px", 'none;"></iframe>"' ]
+
+
+export const vaccineChart = "https://ourworldindata.org/grapher/covid-vaccination-policy"
+
+export const covidVaccineAge = "https://ourworldindata.org/grapher/covid-vaccine-age"
+
+export const youngestAgeCovidVaccination = "https://ourworldindata.org/grapher/youngest-age-covid-vaccination"
