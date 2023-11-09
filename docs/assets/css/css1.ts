4f055c62c24d40a14704680acbf09ba5b144ea8c
Shlok Nangia <shlok_nangia@sfu.ca>
2023-11-09 10:08:46 -0800

    updating build

--- docs/assets/css/css1.ts
+++ docs/assets/css/css1.ts
@@ -6,3 +6,11 @@ export const vaccineChart = "https://ourworldindata.org/grapher/covid-vaccinatio
 export const covidVaccineAge = "https://ourworldindata.org/grapher/covid-vaccine-age"
 
 export const youngestAgeCovidVaccination = "https://ourworldindata.org/grapher/youngest-age-covid-vaccination"
+
+export const internalMovements = "../../assets/internal-movements-calendar-heatmap.html"
+
+export const createPublicTransport ="../../assets/public-transport-covid-calendar-heatmap.html"
+
+export const scatterPlotByCountry = "../../assets/covid-vaccination-policy.html"
+
+export const trendsForCountryData = "../../assets/covid-vaccination-policy-line.html"
--- my-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-import { registerLicense } from '@syncfusion/ej2-base';
-
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cWWdCf1FpQnxbf1xzZFBMYl1bRHBPIiBoS35RdURiWXdec3dcRGhbWEZ0');
-
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
